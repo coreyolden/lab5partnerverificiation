0432426e8762ba186ff8e1ce0f3fca31be7c3788
gskil <gskil@10.80.147.164>
2017-03-16 15:33:33 -0400

    V 2.0

--- Lab5partnerverification/src/main.java
+++ Lab5partnerverification/src/main.java
@@ -4,6 +4,7 @@ public class main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 			System.out.println("words");
+			System.out.println("Hi this is Greg");
 	}
 
 }
